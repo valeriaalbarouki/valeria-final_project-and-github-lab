254b028b35ab722179b793e57cada089b8fcc64a
valeriaalbarouki <tus03897@temple.edu>
2024-12-08 18:44:12 -0500

    Update serialize.lua

--- lib/knife/serialize.lua
+++ lib/knife/serialize.lua
@@ -5,17 +5,14 @@ return function (value)
     local knownCount = 0
     local writer = {}
 
-    -- get writer delegate for this value's type
     local function getWriter (value)
         return writer[type(value)]
     end
 
-    -- check if a value has a representation yet
     local function isReady (value)
         return type(value) ~= 'table' or ready[value]
     end
 
-    -- serialize tables
     function writer.table (value)
         if known[value] then
             return known[value]
@@ -45,12 +42,10 @@ return function (value)
         return variable
     end
 
-    -- preserve sign bit on NaN, since Lua prints "nan" or "-nan"
     local function writeNan (n)
         return tostring(n) == tostring(0/0) and '0/0' or '-(0/0)'
     end
 
-    -- serialize numbers
     function writer.number (value)
         return value == infinity and '1/0'
             or value == -infinity and '-1/0'
@@ -58,20 +53,16 @@ return function (value)
             or ('%.17G'):format(value)
     end
 
-    -- serialize strings
     function writer.string (value)
         return ('%q'):format(value)
     end
 
-    -- serialize booleans
     writer.boolean = tostring
 
-    -- concatenate array, joined by and terminated with line break
     local function lines (t)
         return #t == 0 and '' or tconcat(t, '\n') .. '\n'
     end
 
-    -- generate serialized result
     local write = getWriter(value)
     local result = write and write(value) or 'nil'
     return lines(intro) .. lines(outro) .. 'return ' .. result
