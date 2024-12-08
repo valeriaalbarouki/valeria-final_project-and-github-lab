b843a2e129571292e96b97e2bafeaf5c801900d1
valeriaalbarouki <tus03897@temple.edu>
2024-12-08 17:02:32 -0500

    Update LevelMaker.lua

--- src/LevelMaker.lua
+++ src/LevelMaker.lua
@@ -82,19 +82,18 @@ function LevelMaker.generate(width, height)
                             width = 16,
                             height = 16,
                             
-                            -- select random frame from bush_ids whitelist, then random row for variance
+                            
                             frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7,
                             collidable = false
                         }
                     )
                 end
                 
-                -- pillar tiles
                 tiles[5][x] = Tile(x, 5, tileID, topper, tileset, topperset)
                 tiles[6][x] = Tile(x, 6, tileID, nil, tileset, topperset)
                 tiles[7][x].topper = nil
             
-            -- chance to generate bushes
+            
             elseif math.random(8) == 1 then
                 table.insert(objects,
                     GameObject {
@@ -122,21 +121,21 @@ function LevelMaker.generate(width, height)
                             width = 16,
                             height = 16,
                             
-                            -- select random frame from bush_ids whitelist, then random row for variance
+                            
                             frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7,
                             collidable = false
                         }
                     )
                 end
                 
-                -- pillar tiles
+                
                 tiles[4][x] = Tile(x, 4, tileID, topper, tileset, topperset)
                 
             elseif math.random(8) == 1 then
                 blockHeight = 0
                 id = TALLPILLAR
                 
-                -- chance to generate bush on pillar
+               
                 if math.random(8) == 1 then
                     table.insert(objects,
                         GameObject {
@@ -146,14 +145,14 @@ function LevelMaker.generate(width, height)
                             width = 16,
                             height = 16,
                             
-                            -- select random frame from bush_ids whitelist, then random row for variance
+                           
                             frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7,
                             collidable = false
                         }
                     )
                 end
                 
-                -- pillar tiles
+                
                 tiles[4][x] = Tile(x, 4, tileID, topper, tileset, topperset)
                 tiles[5][x] = Tile(x, 5, tileID, nil, tileset, topperset)
                 tiles[6][x] = Tile(x, 6, tileID, nil, tileset, topperset)
@@ -161,13 +160,13 @@ function LevelMaker.generate(width, height)
 
             end
 
-            -- chance to spawn a block
+            
             if math.random(10) == 1 and blockHeight ~= 0 then
                 bumptimes = math.random(3)
                 print(bumptimes)
                 table.insert(objects,
 
-                    -- jump block
+                    
                     GameObject {
                         texture = 'jump-blocks',
                         x = (x - 1) * TILE_SIZE,
@@ -175,28 +174,28 @@ function LevelMaker.generate(width, height)
                         width = 16,
                         height = 16,
 
-                        -- make it a random variant
+                       
                         frame = math.random(#JUMP_BLOCKS),
                         -- bumptimes = frame,
                         collidable = true,
                         hit = false,
                         solid = true,
 
-                        -- collision function takes itself
+                       
                         onCollide = function(obj)
 
                             bumptimes = bumptimes - 1
 
-                            -- spawn a gem if we haven't already hit the block
+                           
                             -- if not obj.hit then
                             if bumptimes == 0 and not obj.hit then
 
-                                -- chance to spawn gem, not guaranteed
+                                
                                 if math.random(1) == 1 then
 
                                     bumptimes = math.random(3)
 
-                                    -- maintain reference so we can set it to nil
+                                    
                                     local gem = GameObject {
                                         texture = 'gems',
                                         x = (x - 1) * TILE_SIZE,
@@ -208,14 +207,14 @@ function LevelMaker.generate(width, height)
                                         consumable = true,
                                         solid = false,
 
-                                        -- gem has its own function to add to the player's score
+                                       
                                         onConsume = function(player, object)
                                             gSounds['pickup']:play()
                                             player.score = player.score + 100
                                         end
                                     }
                                     
-                                    -- make the gem move up from the block and play a sound
+                                    
                                     Timer.tween(0.1, {
                                         [gem] = {y = (blockHeight - 2) * TILE_SIZE}
                                     })
